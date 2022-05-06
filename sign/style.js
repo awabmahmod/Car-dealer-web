23f8bc81082ec6c933fb211e3a4ccf2680fc2ad5
awabmahmod <41947112+awabmahmod@users.noreply.github.com>
2022-05-06 06:27:10 +0200

    Update style.js

--- sign/style.js
+++ sign/style.js
@@ -1,14 +1,15 @@
-function validate()
-{
-let username =document.getElementById("name").value;
-let Password =document.getElementById("password").value;
-if(username=='alaa' && Password=='123456789')
-{
-    window.open('profile.html');
-}
-else
-{
-alert( " log in failed");
-}
+function validate() {
+    let username = document.getElementById("name").value;
+    let Password = document.getElementById("password").value;
+    if (username == 'alaa' && Password == '123456789') {
+        window.open("../profile.html");
+    }
+    else if (username == localStorage.getItem("name1") && Password == localStorage.getItem("pass1")) {
+        window.open("../profile2.html");
+
+    }
+    else {
+        alert(" log in failed");
+    }
 
 }
