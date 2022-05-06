99b0a6b571255d2890ae3b9ee05250ae8b5cb0e9
awabmahmod <41947112+awabmahmod@users.noreply.github.com>
2022-05-06 06:50:54 +0200

    Update style.js

--- sign/style.js
+++ sign/style.js
@@ -2,7 +2,7 @@ function validate() {
     let username = document.getElementById("name").value;
     let Password = document.getElementById("password").value;
     if (username == 'alaa' && Password == '123456789') {
-        window.open("../profile.html");
+        window.open('profile.html');
     }
     else if (username == localStorage.getItem("name1") && Password == localStorage.getItem("pass1")) {
         window.open('profile2.html');
